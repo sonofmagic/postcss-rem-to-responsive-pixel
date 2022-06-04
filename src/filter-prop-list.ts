13f326a7fa528fef9444829da5f63d249b63ca49
SonOfMagic <1324318532@qq.com>
2022-06-04 15:55:10 +0800

    v5.1.3

--- src/filter-prop-list.ts
+++ src/filter-prop-list.ts
@@ -1,59 +1,71 @@
 /* eslint-disable no-useless-escape */
-export function exact (list: string[]) {
+export function exact(list: string[]) {
   return list.filter((m) => {
     return m.match(/^[^\*\!]+$/)
   })
 }
 
-export function contain (list: string[]) {
+export function contain(list: string[]) {
   return list
     .filter(function (m) {
       return m.match(/^\*.+\*$/)
     })
     .map(function (m) {
-      return m.substr(1, m.length - 2)
+      return m.slice(1, m.length - 1)
     })
 }
 
-export function endWith (list:string[]) {
-  return list.filter(function (m) {
-    return m.match(/^\*[^\*]+$/)
-  }).map(function (m) {
-    return m.substr(1)
-  })
+export function endWith(list: string[]) {
+  return list
+    .filter(function (m) {
+      return m.match(/^\*[^\*]+$/)
+    })
+    .map(function (m) {
+      return m.slice(1)
+    })
 }
-export function startWith (list:string[]) {
-  return list.filter(function (m) {
-    return m.match(/^[^\*\!]+\*$/)
-  }).map(function (m) {
-    return m.substr(0, m.length - 1)
-  })
+export function startWith(list: string[]) {
+  return list
+    .filter(function (m) {
+      return m.match(/^[^\*\!]+\*$/)
+    })
+    .map(function (m) {
+      return m.slice(0, m.length - 1)
+    })
 }
-export function notExact (list:string[]) {
-  return list.filter(function (m) {
-    return m.match(/^\![^\*].*$/)
-  }).map(function (m) {
-    return m.substr(1)
-  })
+export function notExact(list: string[]) {
+  return list
+    .filter(function (m) {
+      return m.match(/^\![^\*].*$/)
+    })
+    .map(function (m) {
+      return m.slice(1)
+    })
 }
-export function notContain (list:string[]) {
-  return list.filter(function (m) {
-    return m.match(/^\!\*.+\*$/)
-  }).map(function (m) {
-    return m.substr(2, m.length - 3)
-  })
+export function notContain(list: string[]) {
+  return list
+    .filter(function (m) {
+      return m.match(/^\!\*.+\*$/)
+    })
+    .map(function (m) {
+      return m.slice(2, m.length - 1)
+    })
 }
-export function notEndWith (list:string[]) {
-  return list.filter(function (m) {
-    return m.match(/^\!\*[^\*]+$/)
-  }).map(function (m) {
-    return m.substr(2)
-  })
+export function notEndWith(list: string[]) {
+  return list
+    .filter(function (m) {
+      return m.match(/^\!\*[^\*]+$/)
+    })
+    .map(function (m) {
+      return m.slice(2)
+    })
 }
-export function notStartWith (list:string[]) {
-  return list.filter(function (m) {
-    return m.match(/^\![^\*]+\*$/)
-  }).map(function (m) {
-    return m.substr(1, m.length - 2)
-  })
+export function notStartWith(list: string[]) {
+  return list
+    .filter(function (m) {
+      return m.match(/^\![^\*]+\*$/)
+    })
+    .map(function (m) {
+      return m.slice(1, m.length - 1)
+    })
 }
